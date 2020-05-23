9cebcc73602b6690a93b2baa63e85446649b407e
Dannel Albert <cuebix@gmail.com>
2020-05-23 13:24:43 -0400

    - unused import

--- dev/Dev.test.js
+++ dev/Dev.test.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Dev } from './Dev'
-import { Boilerplate } from '../src/Boilerplate'
 
 describe('Dev', () => {
   it('should render a Boilerplate component', () => {
