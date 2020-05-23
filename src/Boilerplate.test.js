16bdb025747fbc7172e39ebe382222d43d588d55
Dannel Albert <cuebix@gmail.com>
2020-05-23 14:52:32 -0400

    + @testing-library/user-event

--- src/Boilerplate.test.js
+++ src/Boilerplate.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import { Boilerplate } from './Boilerplate'
 
 describe('Boilerplate', () => {
