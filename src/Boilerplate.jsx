582f7a0bc02e6666ffe21448f3386190971de56e
Dannel Albert <cuebix@gmail.com>
2020-06-15 18:08:53 -0400

    + module export test

--- src/Boilerplate.jsx
+++ src/Boilerplate.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Boilerplate = () => (
-  <div>
-    Boilerplate
-  </div>
-)
+export const Boilerplate = () => {
+  return (
+    <div>
+      Boilerplate
+    </div>
+  )
+}
